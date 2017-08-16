6bd16f1f714ce79506fda6398b76e45181ef1be5
lorax79 <jordan.smith@powerhell.org>
2017-08-16 14:41:46 -0700

    initial release

---
+++ customScript.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+yum -y install tcsh
+
+yum -y install emacs
+
+yum -y install libelf-devel
+
+yum -y install libelf
+
+yum -y install elfutils-libelf
+
+yum -y install elfutils-libelf-devel
+
+yum -y install ia32-libs
+
+yum -y install glibc.i686
+
+yum -y install gcc
+
+yum -y install libgcc
+
+yum -y install libgcc.i686
--- deploy-bash.sh
+++ deploy-bash.sh
@@ -1,17 +1,54 @@
 #!/bin/sh
+#Clone the git repository and run this script from the directory after modifying the variables here and parameters in the parameters files
+#Run 'az login' to authenticate to an Azure Subscription from the CLI prior to running this script
+#Azure CLI v2.0 required for this script to work https://docs.microsoft.com/en-us/cli/azure/install-azure-cli
 
-#Resource Group Name
-RG=DVQuesta
-
-SUBSCRIPTIONNAME=
+#VM Resource Group Name
+VMRG=DVQuestaVms
+#Resource Group Name for the VNET
+VNETRG=DVQVNETRG
+#Name of the deployment subscription
+SUBSCRIPTIONNAME=jorsmith-labsub1
 LOCATION=eastus2
-TEMPLATEPATH=.
-PARAMPATH=.
+VNETTEMPLATEFILE=./simplevnet.json
+VNETPARAMSFILE=./simplevnet.parameters.json
+VMTEMPLATEFILE=./CentOS-ExistingNetwork.json
+VMPARAMSFILE=./CentOS-existing-params.json
+PUBLICIPNAME=dvqpublicIp
+DNSNAME=dvqonazure
 
 az account set --subscription $SUBSCRIPTIONNAME
 echo "Account set to $SUBSCRIPTIONNAME"
-az account show -o table
 
-az group create --name $RG --location $LOCATION
+echo "Bulding VNET ResourceGroup $VNETRG..."
 
-az group deployment create -g $RG --template-file $TEMPLATEPATH/avm-base-CentOS.json --parameters @$PARAMPATH/dvquesta.json
+az group create --name $VNETRG --location $LOCATION
+
+echo "Deploying the VNET..."
+
+az group deployment create -n vnetDeployment -g $VNETRG --template-file $VNETTEMPLATEFILE --parameters @$VNETPARAMSFILE
+az group deployment wait -n vnetDeployment -g $VNETRG --created
+
+echo "Building the VM ResourceGroup $VMRG..."
+az group create --name $VMRG --location $LOCATION
+
+echo "Deploying VMs..."
+az group deployment create -n vmdeployment -g $VMRG --template-file $VMTEMPLATEFILE --parameters @$VMPARAMSFILE
+
+
+echo "Deploying Public IP..."
+az network public-ip create -g $VNETRG -n $PUBLICIPNAME --dns-name $DNSNAME --allocation-method Dynamic
+
+SUBSCRIPTIONID=$(az account show --query "id" | tr -d '"')
+NICNAME=$(az network nic list -g $VMRG --query "[0].name" | tr -d '"')
+IPCONFIG=$(az network nic show -n $NICNAME -g $VMRG --query "ipConfigurations[0].name" | tr -d '"')
+
+az group deployment wait -n vmdeployment -g $VMRG --created
+echo "Attaching Public IP to VM"
+az network nic ip-config update -g $VMRG --nic-name $NICNAME --public-ip-address /subscriptions/$SUBSCRIPTIONID/resourceGroups/$VNETRG/providers/Microsoft.Network/publicIPAddresses/$PUBLICIPNAME --name $IPCONFIG
+
+FQDN=$(az network public-ip show -n $PUBLICIPNAME -g $VNETRG --query "dnsSettings.fqdn")
+IP=$(az network public-ip show -n $PUBLICIPNAME -g $VNETRG --query "ipAddress")
+
+echo "All resources are complete.  Please login via SSH to $FQDN or $IP"
+
